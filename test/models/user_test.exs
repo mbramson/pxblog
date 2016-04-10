35e81449cc4b53a1c4f263532b8958b325e8bfc7
Mathew Bramson <mathewbramson@gmail.com>
2016-04-10 00:29:50 -0400

    initial commit

---
+++ test/models/user_test.exs
@@ -0,0 +1,25 @@
+defmodule Pxblog.UserTest do
+  use Pxblog.ModelCase
+
+  alias Pxblog.User
+
+  @valid_attrs %{email: "test@test.com", username: "testuser",
+                 password: "test1234", password_confirmation: "test1234"}
+  @invalid_attrs %{}
+
+  test "changeset with valid attributes" do
+    changeset = User.changeset(%User{}, @valid_attrs)
+    assert changeset.valid?
+  end
+
+  test "changeset with invalid attributes" do
+    changeset = User.changeset(%User{}, @invalid_attrs)
+    refute changeset.valid?
+  end
+
+  test "password_digest value gets set to a hash" do
+    changeset = User.changeset(%User{}, @valid_attrs)
+    assert Ecto.Changeset.get_change(changeset, :password_digest) == "ABCDE"
+  end
+
+end
